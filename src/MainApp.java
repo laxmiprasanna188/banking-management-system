95e8ed1e41adfee5702880d2c7944f62e27ee625
laxmiprasanna188 <laxmiprasannamadam57@gmail.com>
2026-08-17 21:41:12 +0530

    Created initial banking project structure

---
+++ src/MainApp.java
@@ -0,0 +1,10 @@
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("   BANKING MANAGEMENT SYSTEM");
+        System.out.println("=================================");
+        System.out.println("Project setup successful!");
+    }
+}
